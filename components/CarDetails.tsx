340d6c97d5bb77970d878e5cf74456f6b539dc73
Amandeep Kalakar <amankumaradk@gmail.com>
2023-06-25 13:21:52 +0530

    code updated

---
+++ components/CarDetails.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { CarProps } from "@/types";
+
+interface CarDetailsProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  car: CarProps;
+}
+
+const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
+  return <div>CarDetails</div>;
+};
+
+export default CarDetails;
